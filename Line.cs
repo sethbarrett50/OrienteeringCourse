ff11be650516289738942454ff644093f48115b7
Seth T Barrett <54868870+sethbarrett50@users.noreply.github.com>
2020-02-12 14:51:36 -0500

    Add files via upload

---
+++ Line.cs
@@ -0,0 +1,78 @@
+﻿//This class was created by Seth Barrett, and is made to represent the starting line of an orienteering course.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orienteering_Course_Solver
+{
+    class Line
+    {
+        Point[] groundLine;
+        public int _numLinePts
+        {
+            get;set;
+        }
+        public int _lineLength
+        {
+            get;set;
+        }
+        public int _lineDirection
+        {
+            get; set;
+        }
+        //These initialize my Point array and several properties.
+        public void MakeLine()
+        {
+            double _ftBetweenPts = (double)_lineLength / (double)_numLinePts;
+            groundLine = new Point[_numLinePts];
+            for(int i = 0; i< groundLine.Length; i++)groundLine[i] = new Point();
+            for(int i = 2; i < groundLine.Length; i++)
+            {
+                Point NextPoint = groundLine[i - 1].Move(_ftBetweenPts, _lineDirection);
+                groundLine[i]._xCoor = NextPoint._xCoor;
+                groundLine[i]._yCoor = NextPoint._yCoor;
+            }
+            groundLine[_numLinePts - 1].Move(_ftBetweenPts, _lineDirection);//This is done to include last point object in the array to move to the correct coordinates.
+        }
+        //This method creates an array of points using user input to create a line of points to represent the start line.
+        public string Show(int i)
+        {
+            if (groundLine.Length <= 26)return $"{Convert.ToChar(i + 65)}:{groundLine[i].Show()} ";
+            else return $"{i + 1}:{groundLine[i].Show()} ";
+        }
+        //This method shows a single point in the point array of a line object, and gives a different message depending on the length of the line.
+        public string ShowAll()
+        {
+            string output = null;
+            for(int i = 0; i < groundLine.Length; i++)output += this.Show(i);
+            return output;
+        }
+        //This method uses the Show method to display all the points in the line object's point array.
+        public Point GetPt(int _iP)
+        {
+            Console.WriteLine(_iP);
+            return groundLine[_iP];
+        }
+        //This method returns a point based upon the parameter given.
+        public string Closest(Point pointP)
+        {
+            double[] DistanceTo = new double[this._numLinePts];
+            for(int i = 0; i < groundLine.Length; i++)
+            {
+                DistanceTo[i] = Math.Sqrt(Math.Pow((double)groundLine[i]._xCoor + pointP._xCoor, 2) + Math.Pow((double)groundLine[i]._yCoor + pointP._yCoor,2));
+            }
+            int _closestPt = 0;
+            for(int i = 1; i < groundLine.Length; i++)
+            {
+                if(DistanceTo[i] < DistanceTo[i - 1])
+                {
+                    _closestPt = i;
+                }
+            }
+            return $"The point on the groundline that is the closest to your end point is {groundLine[_closestPt].Show()} ";
+        }
+        //This method takes a point as a parameter and scans the point array in this line object and returns a string telling which point is closest in the array.
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * This project's goal is to solve the Klondike Orienteering Course. 
+ * Created by Seth Barrett.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orienteering_Course_Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Line groundLine = new Line();
+            Course OC;
+            //Initializes course object and line object.
+
+            Console.WriteLine("How many points are on the ground line?");
+            groundLine._numLinePts = UserInputVeri(1, 100, false);
+            Console.WriteLine("How long is the ground line in feet?");
+            groundLine._lineLength = UserInputVeri(1, 250, false);
+            Console.WriteLine("What direction in degrees does the line follow, facing from the starting value?");
+            groundLine._lineDirection = UserInputVeri(0, 359, false);
+            //Takes the user's inputs and verifies them before setting the groundLine's values.
+
+            groundLine.MakeLine();
+            Console.WriteLine($"The starting line is represented by these coordinates:{groundLine.ShowAll()}");
+            //Creates the points and their values for the starting line.
+
+            Console.WriteLine("Which point are you starting at?");
+            OC = new Course(groundLine.GetPt(UserInputVeri(0, groundLine._numLinePts, groundLine._numLinePts <= 26)));
+            Console.WriteLine("How many instructions are there?");
+            OC._numDirections = UserInputVeri(0, 1000, false);
+            //Takes the user's inputs and verifies them before setting the OC's values.
+
+            Console.WriteLine(groundLine.Closest(OC.FindEnd()));
+            //This runs through all the directions for OC and write the Point on groundLine that is the closest to it.
+        }
+        static int UserInputVeri(int lowerBound, int upperBound, bool letter)
+        {
+            bool flag = false;
+            bool realLet = false;
+            int output = 0;
+            char charput;
+            if (letter)
+            {
+                while (!flag)
+                {
+                    string input = Console.ReadLine();
+                    realLet = char.TryParse(input, out charput);
+                    if (realLet)
+                    {
+                        int place = (int)charput;
+                        if (place >= 65 && place <= 90)return output = place - 65;
+                        else if(place >= 97 && place <= 122)return output = place - 97;
+                        else flag = int.TryParse(input, out output);
+                    }
+                    else flag = int.TryParse(input, out output);
+
+                    if (!flag || output < lowerBound || output > upperBound) Console.WriteLine("Please enter a valid value");
+                }
+            }
+            else
+            {
+                while (!flag || output < lowerBound || output > upperBound)
+                {
+                    flag = int.TryParse(Console.ReadLine(), out output);
+                    if (!flag || output < lowerBound || output > upperBound) Console.WriteLine("Please enter a valid value");
+                }
+            }
+            return output;
+        }
+        //This method does user input verification for allowed int values and certain char values based upon the parameters.
+    }
+}
